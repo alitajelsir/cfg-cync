479d391f267cea0fbc5b2b419d58543cf9462dbf
Ali Tajelsir <alitajelsir@gmail.com>
2026-06-09 14:57:26 +0300

    Update profile.d environment

--- .config/profile.d/02-applications.sh
+++ .config/profile.d/02-applications.sh
@@ -5,7 +5,6 @@ export GIT_CONFIG_GLOBAL=$XDG_CONFIG_HOME/git/config
 export LESS="$LESS -x2RF"
 export MANPATH=:$PREFIX/share/man:$XDG_DATA_HOME/man
 export RIPGREP_CONFIG_PATH=$XDG_CONFIG_HOME/rg/config
-export XZ_DEFAULTS='-T0'
 
 if [[ $UID != 0 ]]; then
 	fd . -t l --max-depth=1 $HOME -X rm
