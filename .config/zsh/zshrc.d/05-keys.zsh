1f9baa8e524814bb457108a9d699f57a732dc7b0
Ali Tajelsir <alitajelsir@gmail.com>
2026-07-14 03:25:01 +0300

    Update zsh configuration

--- .config/zsh/zshrc.d/05-keys.zsh
+++ .config/zsh/zshrc.d/05-keys.zsh
@@ -9,12 +9,12 @@ bindkey '^Xa' _expand_alias
 
 # Bind Alt+shift+s to prefix with sudo
 () {
-  bindkey '^[S' $1
-  zle -N $1
-  $1() {
-    [[ $BUFFER == [[:space:]]# ]] &&
-      zle .up-history
+	bindkey '^[S' $1
+	zle -N $1
+	$1() {
+	  [[ $BUFFER == [[:space:]]# ]] &&
+	    zle .up-history
 
-    LBUFFER=" sudo $LBUFFER"
-  }
+	  LBUFFER=" sudo $LBUFFER"
+	}
 } .sudo
