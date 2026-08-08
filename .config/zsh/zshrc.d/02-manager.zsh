e2a040afa6723e27315da3e2458a9d13c3e66830
Ali Tajelsir <alitajelsir@gmail.com>
2026-08-08 17:34:48 +0300

    Update zsh configuration

--- .config/zsh/zshrc.d/02-manager.zsh
+++ .config/zsh/zshrc.d/02-manager.zsh
@@ -6,8 +6,10 @@
 		_manger_remote=https://github.com/mattmc3/antidote.git
 
 # Install antidote
-	[[ -r $_manger_local/antidote.zsh ]] ||
-		git clone --depth 1 $_manger_remote $_manger_local
+	if [[ ! -r $_manger_local/antidote.zsh ]]; then
+		printf '# zsh cloning mattmc3/antidote...\n'
+		git clone --quiet --depth 1 $_manger_remote $_manger_local
+	fi
 	source $_manger_local/antidote.zsh
 }
 
@@ -16,3 +18,4 @@ export ANTIDOTE_HOME=$XDG_DATA_HOME/zsh/plugins
 zstyle ':antidote:bundle' file $ZDOTDIR/.zplugins
 zstyle ':antidote:static' file $XDG_STATE_HOME/zsh/zplugins.zsh
 zstyle ':antidote:bundle' path-style short
+zstyle ':antidote:bundle:*' shallow true
