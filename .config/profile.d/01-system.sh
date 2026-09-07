59ad3dd2a19b80bdefde4716378592c36973be01
Ali Tajelsir <alitajelsir@gmail.com>
2026-09-07 18:05:39 +0300

    Update profile.d configuration

--- .config/profile.d/01-system.sh
+++ .config/profile.d/01-system.sh
@@ -6,15 +6,15 @@ export LC_CTYPE=en_US.UTF-8
 export PATH="$PATH:/data/adb/ap/bin:/data/adb/ksu/bin:$HOME/.local/bin"
 
 # Set XDG user directories
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_DATA_HOME=$HOME/.local/share
-export XDG_STATE_HOME=$HOME/.local/state
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
 
 # Set apt configuration file
-export APT_CONFIG=$XDG_CONFIG_HOME/apt/apt.conf
+export APT_CONFIG="$XDG_CONFIG_HOME/apt/apt.conf"
 
 # Fix ownership
-[[ $UID == 0 ]] || 
+[[ "$(id -u)" == 0 ]] || 
 	(find "$HOME" "$PREFIX" -user root \
 		-exec sudo chown "$USERNAME:$USERNAME" {} + &>/dev/null &)
--- .config/profile.d/02-applications.sh
+++ .config/profile.d/02-applications.sh
@@ -1,11 +1,11 @@
 export EDITOR=nvim
-export FFMPEG_DATADIR=$XDG_CONFIG_HOME/ffmpeg
-export GIT_CONFIG_GLOBAL=$XDG_CONFIG_HOME/git/config
+export FFMPEG_DATADIR="$XDG_CONFIG_HOME/ffmpeg"
+export GIT_CONFIG_GLOBAL="$XDG_CONFIG_HOME/git/config"
 export LESS="$LESS -x2RF"
-export MANPATH=:$PREFIX/share/man:$XDG_DATA_HOME/man
-export RIPGREP_CONFIG_PATH=$XDG_CONFIG_HOME/rg/config
+export MANPATH="$MANPATH:$PREFIX/share/man:$XDG_DATA_HOME/man"
+export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/rg/config"
 
-if [[ $UID != 0 ]]; then
-	fd . -t l --max-depth=1 $HOME -X rm
-	ln -sf /storage/emulated/0/* $HOME
+if [[ "$(id -u)" != 0 ]]; then
+	find "$HOME" -maxdepth 1 -type l -exec rm {} +
+	ln -sf /storage/emulated/0/* "$HOME"
 fi
