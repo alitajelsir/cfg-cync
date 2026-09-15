ceec3a13a786f439e3fb0e1e62d0e3012e93fa7d
Ali Tajelsir <alitajelsir@gmail.com>
2026-09-15 11:54:28 +0300

    Update profile.d configuration

--- .config/profile.d/02-applications.sh
+++ .config/profile.d/02-applications.sh
@@ -1,7 +1,7 @@
 export EDITOR=nvim
 export FFMPEG_DATADIR="$XDG_CONFIG_HOME/ffmpeg"
 export GIT_CONFIG_GLOBAL="$XDG_CONFIG_HOME/git/config"
-export LESS="$LESS -x2RF"
+export LESS="$LESS -x2R"
 export MANPATH="$MANPATH:$PREFIX/share/man:$XDG_DATA_HOME/man"
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/rg/config"
 
